981dc1466265058484e1ca786720d3ec54fe4639
Rizwan Ali <rizcube@gmail.com>
2015-05-26 12:47:56 +0100

    it can do the basic calculation

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 var a,b;
-a = Number(document.getElementById("a").value;)
-b = Number(document.getElementById("b").value);
+
+function setValues() {
+  a = Number(document.getElementById("a").value);
+  b = Number(document.getElementById("b").value);
+}
+
+function sum() {
+  setValues();
+  result = a + b;
+  document.write("The sum is equal to " +  result);
+}
+
+function subtract(){
+  setValues();
+  result = a - b;
+  document.write("The subtraction is equal to " + result);
+}
+
+function multiply(){
+  setValues();
+  result = a * b;
+  document.write("The multiplication is equal to " + result);
+}
+
+function divide() {
+  setValues();
+  result = a / b;
+  document.write("The division is equal to " + result);
+}
